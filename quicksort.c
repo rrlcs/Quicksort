1a742ee9aea56856708dab3f6fab19dc8246a693
root <Ravi Raja>
2015-07-17 15:05:28 +0530

    Half of the array sorted successfully using recursive quicksort

--- quicksort.c
+++ quicksort.c
@@ -7,13 +7,13 @@ int main()
 	unsigned int *arr, size;
 	unsigned int pivot, i, big_index, small_index;
 
-	arr = NULL;
+	arr = (unsigned int*)malloc(sizeof(unsigned int)*15);
 	printf("Enter the size of the array :\n");
 	scanf("%d", &size);
 	printf("Enter the array elements : \n");
 	for(i=0; i<size; i++)
 	{
-		arr = (unsigned int*)realloc(arr, i+1);
+
 		scanf("%d", (arr+i));
 	}
 	for(i=0; i<size; i++)
@@ -25,15 +25,15 @@ int main()
 	printf("Enter the Position of pivot element : \n");
 	scanf("%d", &pivot);
 
-	big_index = 1;
+	big_index = pivot + 1;
         small_index = size-1;
 	arr = quicksort(arr, big_index, small_index, size, pivot);
 	
 	for(i=0; i<size; i++)
 	{
 		printf("%d\t", *(arr+i));
-	}	
-//	free(arr);
+	}
+
 	return 0;
 }
 unsigned int *quicksort(unsigned int *arr, unsigned int big_index, unsigned int small_index, unsigned int size, unsigned int pivot)
@@ -74,13 +74,15 @@ GO :	while(*(arr+big_index) <= *(arr+pivot))
 	printf("Position of pivot : %d\n", pivot);
 	printf("Small_index : %d\n", small_index);
 	printf("Big_index : %d\n", big_index);
-	if(small_index > 0)
+	while(small_index > 0)
 	{
 		big_index = pivot + 1;
 		small_index = small_index - 1;
+		size = small_index;
 		printf("Function Calling Again");
 		arr = quicksort(arr, big_index, small_index, size, pivot);
-	}	
+	}
+		
 	if(size > big_index)
 	{
 		pivot = small_index + 1;
